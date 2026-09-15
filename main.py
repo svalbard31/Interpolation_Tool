917e4567d0c74c86e8005d60a57248493ab8c69b
svalbard31 <chfal2192@uit.no>
2026-09-15 18:15:18 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+import numpy.polynomial as poly
+import time 
+
+points = [[0,1],[2,2],[3,4]]
+
+#n^{2} And has to be revaulted every time
+def lagrange_interpol_simple(points,x):
+    y=0
+    for i in range(len(points)):
+        y1 = 1
+        y2 = 1
+        for j in range(len(points)):
+            if i!=j:
+                y1*=x-points[j][0]
+                y2*=points[i][0]-points[j][0]
+        y+=points[i][1]*(y1/y2)
+    return y
+
+#This time using numpy and (hopefully returning a polynomial)
+def lagrange_interpol_numpy(points,x):
+    y=poly.polynomial([0]) # When working in the polynomial ring everthings is polynomial
+    for i in range(len(points)):
+        y1 = poly.polynomial([1]) #Identiy element lol. 
+        for j in range(len(points)):
+            if i!=j:
+                temp = poly.polynomial(points[j][0],1)
+                y1 = poly.polynomial.mul(y1,temp)
+                y2*=points[i][0]-points[j][0]
+        
+
+
+
+
+
+
+def test_func(x):
+    return (0.5*(x**2))-(0.5*x)+1 
+
+x = np.linspace(0,20,100)
+yber = []
+start = time.time()
+for element in x:
+    yber.append(lagrange_interpol_simple(points,element))
+end = time.time()
+print(end-start)
+plt.plot(x,yber)
+start = time.time()
+yfunc2 = test_func(x)
+end = time.time()
+print(end-start)
+plt.plot(x,yfunc2,color = 'red')
+plt.show()
